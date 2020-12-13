c0a7df7e6dc2097ffefba5e89385016ad65d4eda
Tomasz Subik <tsubik@gmail.com>
2020-12-13 19:57:36 +0100

    lot of shit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,161 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+
+import { useEffect, useState } from 'react';
+import Modal from 'react-modal';
+
+function fetchScans(filter) {
+  const searchParams = new URLSearchParams();
+  if (filter) {
+    searchParams.append('filter', JSON.stringify({ q: filter }));
+  }
+  console.log('searchParams', searchParams.toString());
+  return fetch('http://localhost:3000/scans?' + searchParams).then(res => res.json());
+}
+
+function updateScan(scan) {
+  return fetch(`http://localhost:3000/scans/${scan.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(scan)
+  });
+}
+
+function createScan(scan) {
+  return fetch(`http://localhost:3000/scans`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(scan)
+  });
+}
+
+Modal.setAppElement('#root');
+
+function ScanForm({ scan, onSubmit }) {
+  const [edited, setEdited] = useState(scan);
+
+  function handleValueChange(property) {
+    return function (e) {
+      const newScan = { ...edited };
+      newScan[property] = e.target.value;
+      setEdited(newScan);
+    };
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onSubmit && onSubmit(edited);
+  }
+
+  return (
+    <form onSubmit={handleSubmit} noValidate>
+      <input type="text" value={edited.title} className="input" onChange={handleValueChange('title')} /><br/>
+
+      <button type="submit" className="btn btn-blue">
+        Save
+      </button>
+    </form>
+  );
+}
 
 function App() {
+  const [filter, setFilter] = useState('');
+  const [scans, setScans] = useState([]);
+  const [editOpen, setEditOpen] = useState(false);
+  const [newOpen, setNewOpen] = useState(false);
+  const [newScan, setNewScan] = useState({});
+  const [editedScan, setEditedScan] = useState();
+
+  useEffect(() => {
+    updateList();
+  }, [])
+
+  async function updateList(filter) {
+    const data = await fetchScans(filter);
+    setScans(data);
+  }
+
+  function closeModal(){
+    setEditOpen(false);
+    setNewOpen(false);
+  }
+
+  function openEditModal(scan) {
+    setEditedScan(scan);
+    setEditOpen(true);
+  }
+
+  function handleSearch(e) {
+    e.preventDefault();
+    updateList(filter);
+  }
+
+  async function handleEdit(scan) {
+    await updateScan(scan);
+    updateList();
+    setEditOpen(false);
+  }
+
+  async function handleNew(scan) {
+    await createScan(scan);
+    updateList();
+    setNewOpen(false);
+  }
+
+  async function handleAdd() {
+    setNewScan({});
+    setNewOpen(true);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <form onSubmit={handleSearch} noValidate className="p-2 bg-gray-300 rounded">
+        <label>
+          Filter:
+        </label>
+        <input type="text" className="ml-3 input" value={filter} onChange={(e) => setFilter(e.target.value)} />
+
+        <button type="submit" className="ml-3 btn btn-blue">
+          Search
+        </button>
+      </form>
+
+      {scans.map((scan) => (
+        <div className="p-2 my-2 border border-gray-700 shadow-lg rounded" onClick={openEditModal.bind(this, scan)}>
+          {scan.title}
+        </div>
+      ))}
+
+      <button type="button" className="btn btn-blue" onClick={handleAdd}>
+        Add new
+      </button>
+
+      {editedScan && (
+        <Modal
+          isOpen={editOpen}
+          onRequestClose={closeModal}
+          contentLabel="Example Modal"
         >
-          Learn React
-        </a>
-      </header>
+          <h2>Edit Scan</h2>
+          <button onClick={closeModal}>close</button>
+          <ScanForm scan={editedScan} onSubmit={handleEdit} />
+        </Modal>
+      )}
+
+      {newScan && (
+        <Modal
+          isOpen={newOpen}
+          onRequestClose={closeModal}
+          contentLabel="Example Modal"
+        >
+          <h2>New Scan</h2>
+          <button onClick={closeModal}>close</button>
+          <ScanForm scan={newScan} onSubmit={handleNew} />
+        </Modal>
+      )}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import fetchMock from 'fetch-mock';
+import FakeRest from 'fakerest';
+
+const data = {
+  patients: [
+    { id: 1, name: 'Tomasz', email: 'tsubik@gmail.com', gender: 'M' },
+    { id: 2, name: 'Edyta', email: 'edyta@example.com', gender: 'F' }
+  ],
+  scans: [
+    { id: 1, patient_id: 1, title: 'My first scan' },
+    { id: 2, patient_id: 1, title: 'My second scan' },
+    { id: 3, patient_id: 1, title: 'My third scan' },
+    { id: 4, patient_id: 1, title: 'My third scan' },
+    { id: 5, patient_id: 1, title: 'My third scan' },
+    { id: 6, patient_id: 1, title: 'My third scan' },
+    { id: 7, patient_id: 1, title: 'My third scan' },
+    { id: 8, patient_id: 1, title: 'My third scan' },
+    { id: 9, patient_id: 1, title: 'My third scan' },
+    { id: 10, patient_id: 1, title: 'My third scan' },
+  ]
+};
+
+// initialize fake REST server
+const restServer = new FakeRest.FetchServer('http://localhost:3000');
+restServer.toggleLogging();
+restServer.init(data);
+fetchMock.mock('begin:http://localhost:3000', restServer.getHandler());
 
 ReactDOM.render(
   <React.StrictMode>
